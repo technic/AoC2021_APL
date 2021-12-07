2ac1918e2c47064914cee0a8d814f4c5ff872604
Alex Maystrenko <alexeytech@gmail.com>
2021-12-07 20:54:08 +0100

    add wrapper to stop on error

--- src/run.sh
+++ src/run.sh
@@ -3,6 +3,8 @@ set -e
 for x in $(find . -name '*.dyalog' | sort -V)
 do
     echo "=== Run $x ==="
-    dyalog -script "$x"
+    echo "⎕TRAP←0 'E' '⍪⎕DM ⋄ ⎕OFF 1'" > tmp.dyalog
+    cat $x >> tmp.dyalog
+    dyalog -script tmp.dyalog
     echo ""
 done
